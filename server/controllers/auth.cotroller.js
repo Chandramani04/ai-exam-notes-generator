928e10a4d4e124b98e457fc5575e513d20fbae33
Chandramani04 <chandramani2701@gmail.com>
2026-05-27 10:09:40 +0530

    fix(cookie) : cookie settings update for production

--- server/controllers/auth.cotroller.js
+++ server/controllers/auth.cotroller.js
@@ -20,8 +20,10 @@ export async function googleAuth(req, res) {
         const token = generateToken({ userid: dbUser._id });
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            // secure: process.env.NODE_ENV === "production", // in development
+            secure: true, // in production
+            // sameSite: "strict", // in development
+            sameSite: "none", // in production
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
         return res.status(200).json({
